43298e53fdfd1f5897327d7d8b7a6865e2de8b15
FedeGothEnjoyer <federicopalmio@gmail.com>
2025-08-14 00:14:47 +0000

    First commit: added a calculation to determine the distance in rgb space and posterize the output to optimize standard  terminal output

---
+++ main.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+#include <sys/ioctl.h>
+#include <thread>
+#include <boost/algorithm/string/join.hpp>
+
+using namespace std;
+
+int SCREEN_WIDTH,SCREEN_HEIGHT;
+chrono::steady_clock::time_point start_time;
+chrono::steady_clock::time_point delta_time_clock;
+
+const int threshold = 80000;
+
+void getTerminalSize(int &x, int &y) {
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    y = w.ws_row;
+    x = w.ws_col;
+}
+
+void build_line (int yb, int ye, vector<string>& buffer) {
+    int lr=1,lg=1,lb=1;
+    for(int y = yb; y < ye; y++){
+        string &line = buffer[SCREEN_HEIGHT - 1 - y];
+        line.clear();
+        for(int x = 0; x < SCREEN_WIDTH; x++){
+            int r = (int)((float)x/SCREEN_WIDTH*255);
+            int g = (int)((float)y/SCREEN_HEIGHT*255);
+            int g2 = (int)((float)(y+0.5f)/SCREEN_HEIGHT*255);
+            int b = (sin(std::chrono::duration_cast<std::chrono::milliseconds>(delta_time_clock-start_time).count()/1000.0)+1.0)*255/2;
+            
+            if(x==0){
+                lr=r;lg=g;lb=b;
+            }
+            else{
+                int dr = r - lr;
+                int dg = g - lg;
+                int db = b - lb;
+                int rmean = (r + lr) >> 1;
+                int dist2 = ((512 + rmean) * dr * dr) + (1024 * dg * dg) + ((512 + (255 - rmean)) * db * db);
+
+                if(dist2<threshold){
+                    line += "█";
+                    continue;
+                }
+                else{
+                    lr=r;lg=g;lb=b;
+                }
+            }
+
+
+            line += "\x1b[38;2;"+to_string(r)+";"+to_string(g)+";"+to_string(b)+"m█";
+        }
+    }
+};
+
+int main(){
+    
+    std::cout << "\x1b[?25l"; //hide cursor
+
+    string output;
+    int cur_fps=0;
+    chrono::steady_clock::time_point fps_timer = std::chrono::steady_clock::now();
+    start_time = std::chrono::steady_clock::now();
+
+
+    for(int cur_frame = 0;;cur_frame++){
+        getTerminalSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        delta_time_clock = std::chrono::steady_clock::now();
+        int delta_time = std::chrono::duration_cast<std::chrono::milliseconds>(delta_time_clock - fps_timer).count();
+        if(delta_time>=500){
+            fps_timer = delta_time_clock;
+            cur_fps = cur_frame*2;
+            cur_frame = 0;
+        }
+
+
+        vector<string> buffer(SCREEN_HEIGHT);
+/*
+        auto build_line = [&](int y) {
+            string line = "";
+            for(int x = 0; x < SCREEN_WIDTH; x++){
+                int r = (int)((float)x/SCREEN_WIDTH*255);
+                int g = (int)((float)y/SCREEN_HEIGHT*255);
+                int g2 = (int)((float)(y+0.5f)/SCREEN_HEIGHT*255);
+                int b = (sin(std::chrono::duration_cast<std::chrono::milliseconds>(delta_time_clock-start_time).count()/1000.0)+1.0)*255/2;
+                
+                line += "\x1b[38;2;"+to_string(r)+";"+to_string(g)+";"+to_string(b)+";48;2;"+to_string(r)+";"+to_string(g2)+";"+to_string(b)+"m▄";
+            }
+            buffer[SCREEN_HEIGHT-3 - y] = std::move(line);
+        };
+*/
+
+
+        int CORES = 12;
+
+        int renderheight = SCREEN_HEIGHT-1;
+        int block_size = renderheight / CORES;
+
+        vector<thread> threads;
+
+        for(int y = 0; y < CORES; y++){
+            threads.emplace_back(build_line, y*block_size, (y==CORES-1?renderheight:(y+1)*block_size), std::ref(buffer));
+        }
+        for(auto &t : threads) t.join();
+
+        output.clear();
+        output.reserve(SCREEN_WIDTH * (SCREEN_HEIGHT - 1) * 23 + 196); // guess a safe size
+
+        output += "\x1b[H\x1b[?25l";
+        output += "\x1b[39;49m" + to_string(SCREEN_WIDTH) + "x" + to_string(SCREEN_HEIGHT*2) + " " + to_string(cur_fps) + " \x1b[K\n";
+        for(auto &i:buffer) output += i;
+        cout << output;
+        cout.flush();
+/*
+
+        cout << "\x1b[H";
+        cout << SCREEN_WIDTH << "x" << SCREEN_HEIGHT << " " << cur_fps << "                                              \n";
+        output.clear();
+        for(int y = SCREEN_HEIGHT-3; y >= 0; y--){
+            for(int x = 0; x < SCREEN_WIDTH; x++){
+                int r = (int)((float)x/SCREEN_WIDTH*255);
+                int b = (int)((float)y/SCREEN_HEIGHT*255);
+                int b2 = (int)((float)(y+0.5f)/SCREEN_HEIGHT*255);
+                output += "\x1b[38;2;"+to_string(r)+";"+to_string(b)+";"+to_string(0)+";48;2;"+to_string(r)+";"+to_string(b2)+";1m▄";
+            }
+        }
+        cout << output << "\x1b[0m";
+*/
+    }
+
+    return 0;
+}
