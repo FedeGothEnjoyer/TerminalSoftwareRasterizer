1959603ea520a762785b01b529a486c7d5771664
FedeGothEnjoyer <federicopalmio@gmail.com>
2025-08-17 23:49:24 +0000

    Modified the main loop to use a thread pool instead of recreating threads every frame, optimized string concatenation and modified the standard to C++ 20

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #include <sys/ioctl.h>
-#include <thread>
+#include <charconv> // put at top of file
 
 //lib
 #include "vec.h"
@@ -9,6 +9,7 @@
 using namespace std;
 
 constexpr float FP_EPSILON = 1e-6f;
+constexpr int CORES = 12;
 
 float2 A={15,5},B={60, 50},C={80, 25},D={150,15};
 
@@ -18,6 +19,9 @@ chrono::steady_clock::time_point delta_time_clock;
 
 const int threshold = 80000;
 
+array<binary_semaphore,CORES>semaphore_full=[]<size_t...Is>(index_sequence<Is...>){return array<binary_semaphore,sizeof...(Is)>{((void)Is,binary_semaphore{0})...};}(make_index_sequence<CORES>());
+array<binary_semaphore,CORES>semaphore_empty=[]<size_t...Is>(index_sequence<Is...>){return array<binary_semaphore,sizeof...(Is)>{((void)Is,binary_semaphore{0})...};}(make_index_sequence<CORES>());
+
 void getTerminalSize(int &x, int &y) {
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
@@ -25,19 +29,19 @@ void getTerminalSize(int &x, int &y) {
     x = w.ws_col;
 }
 
-float PointIsOnRightSideOfLine(float2 a, float2 b, float2 p){ // res<0 == true
+inline float PointIsOnRightSideOfLine(float2 a, float2 b, float2 p){ // res<0 == true
     b-=a;
     p-=a;
     return (b.x*p.y) - (p.x*b.y);
 }
 
-bool EdgeIsTopLeft(const float2 &a, const float2 &b) {
+inline bool EdgeIsTopLeft(const float2 &a, const float2 &b) {
     float dx = b.x - a.x;
     float dy = b.y - a.y;
     return (dy > 0.0f) || (fabsf(dy) < FP_EPSILON && dx < 0.0f);
 }
 
-bool PointIsInsideTriangle(float2 a, float2 b, float2 c, float2 p){
+inline bool PointIsInsideTriangle(float2 a, float2 b, float2 c, float2 p){
     float ab = PointIsOnRightSideOfLine(a, b, p);
     float bc = PointIsOnRightSideOfLine(b, c, p);
     float ca = PointIsOnRightSideOfLine(c, a, p);
@@ -47,87 +51,123 @@ bool PointIsInsideTriangle(float2 a, float2 b, float2 c, float2 p){
            (ca<0||(EdgeIsTopLeft(c,a)&&ca==0));
 }
 
-void build_line (int yb, int ye, vector<string>& buffer) {
-    color last_pixel, last_pixel2;
-    for(int y = yb; y < ye; y++){
-        string &line = buffer[SCREEN_HEIGHT - 1 - y];
-        line.clear();
-        line += "\x1b[38;2;1;1;1m\x1b[48;2;1;1;1m";
-        for(int x = 0; x < SCREEN_WIDTH; x++){
-            color pixel(1,1,1),pixel2(1,1,1);
 
-            if(PointIsInsideTriangle(A, B, C, {(float)x,(float)y})){
-                pixel.r=(int)((float)x/SCREEN_WIDTH*255);
-                pixel.g=(int)((float)y/SCREEN_HEIGHT*255);
-                pixel.b=(sin(std::chrono::duration_cast<std::chrono::milliseconds>(delta_time_clock-start_time).count()/1000.0)+1.0)*255/2;
-            }
-            if(PointIsInsideTriangle(A, B, C, {(float)x,(float)y+0.5f})){
-                pixel2.r=(int)((float)x/SCREEN_WIDTH*255);
-                pixel2.g=(int)((float)(y+0.5f)/SCREEN_HEIGHT*255);
-                pixel2.b=(sin(std::chrono::duration_cast<std::chrono::milliseconds>(delta_time_clock-start_time).count()/1000.0)+1.0)*255/2;
-            }
+void build_line (int yb, int ye, vector<string>& buffer, int id) {
+    for(;;){
+        semaphore_empty[id].acquire();
 
-            if(PointIsInsideTriangle(B, D, C, {(float)x,(float)y})){
-                pixel = color(255,1,1);
-            }
-            if(PointIsInsideTriangle(B, D, C, {(float)x,(float)y+0.5f})){
-                pixel2 = color(255,1,1);
-            }
+        const int sw = SCREEN_WIDTH;
+        color last_pixel, last_pixel2;
 
-            if(PointIsInsideTriangle(A, C, D, {(float)x,(float)y})){
-                pixel = color(1,255,1);
-            }
-            if(PointIsInsideTriangle(A, C, D, {(float)x,(float)y+0.5f})){
-                pixel2 = color(1,255,1);
-            }
+        for(int y = yb; y < ye; ++y){
+            string &line = buffer[buffer.size() - 1 - y];
+            line.clear();
 
-            /*
-            int r = (int)((float)x/SCREEN_WIDTH*255);
-            int g = (int)((float)y/SCREEN_HEIGHT*255);
-            //int g2 = (int)((float)(y+0.5f)/SCREEN_HEIGHT*255);
-            int b = (sin(std::chrono::duration_cast<std::chrono::milliseconds>(delta_time_clock-start_time).count()/1000.0)+1.0)*255/2;
-            */
+            line.append("\x1b[38;2;1;1;1m\x1b[48;2;1;1;1m");
 
+            char numbuf[16];
+            for(int x = 0; x < sw; ++x){
+                color pixel(1,1,1), pixel2(1,1,1);
 
-            if(x==0){
-                last_pixel = pixel;
-                last_pixel2 = pixel2;
-            }
-            else{
-                int dist_sq = ColorDifferenceSquared(pixel, last_pixel);
-                int dist_sq2 = ColorDifferenceSquared(pixel2, last_pixel2);
+                if(PointIsInsideTriangle(A, B, C, {(float)x,(float)y})){
+                    pixel.r = (int)((float)x/sw*255);
+                    pixel.g = (int)((float)y/SCREEN_HEIGHT*255);
+                    pixel.b = 1;
+                }
+                if(PointIsInsideTriangle(A, B, C, {(float)x,(float)y+0.5f})){
+                    pixel2.r = (int)((float)x/sw*255);
+                    pixel2.g = (int)((float)(y+0.5f)/SCREEN_HEIGHT*255);
+                    pixel2.b = 1;
+                }
 
-                if(dist_sq>threshold){
-                    line += "\x1b[38;2;"+to_string(pixel.r)+";"+to_string(pixel.g)+";"+to_string(pixel.b)+"m";
+                if(PointIsInsideTriangle(B, D, C, {(float)x,(float)y})){
+                    pixel = color(255,1,1);
+                }
+                if(PointIsInsideTriangle(B, D, C, {(float)x,(float)y+0.5f})){
+                    pixel2 = color(255,1,1);
+                }
+                if(PointIsInsideTriangle(A, C, D, {(float)x,(float)y})){
+                    pixel = color(1,255,1);
+                }
+                if(PointIsInsideTriangle(A, C, D, {(float)x,(float)y+0.5f})){
+                    pixel2 = color(1,255,1);
+                }
+
+                if(x == 0){
                     last_pixel = pixel;
-                }
-                if(dist_sq2>threshold){
-                    line += "\x1b[48;2;"+to_string(pixel2.r)+";"+to_string(pixel2.g)+";"+to_string(pixel2.b)+"m";
                     last_pixel2 = pixel2;
+                } else {
+                    int dist_sq  = ColorDifferenceSquared(pixel, last_pixel);
+                    int dist_sq2 = ColorDifferenceSquared(pixel2, last_pixel2);
+
+                    if(dist_sq > threshold){
+                        line.append("\x1b[38;2;");
+                        auto res = std::to_chars(numbuf, numbuf + sizeof(numbuf), pixel.r);
+                        line.append(numbuf, res.ptr - numbuf);
+                        line.push_back(';');
+                        res = std::to_chars(numbuf, numbuf + sizeof(numbuf), pixel.g);
+                        line.append(numbuf, res.ptr - numbuf);
+                        line.push_back(';');
+                        res = std::to_chars(numbuf, numbuf + sizeof(numbuf), pixel.b);
+                        line.append(numbuf, res.ptr - numbuf);
+                        line.append("m");
+                        last_pixel = pixel;
+                    }
+                    if(dist_sq2 > threshold){
+                        line.append("\x1b[48;2;");
+                        auto res = std::to_chars(numbuf, numbuf + sizeof(numbuf), pixel2.r);
+                        line.append(numbuf, res.ptr - numbuf);
+                        line.push_back(';');
+                        res = std::to_chars(numbuf, numbuf + sizeof(numbuf), pixel2.g);
+                        line.append(numbuf, res.ptr - numbuf);
+                        line.push_back(';');
+                        res = std::to_chars(numbuf, numbuf + sizeof(numbuf), pixel2.b);
+                        line.append(numbuf, res.ptr - numbuf);
+                        line.append("m");
+                        last_pixel2 = pixel2;
+                    }
                 }
-            }
-            line += "▄";
-        }
+
+                line+="▄";
+            } // x
+        } // y
+
+        semaphore_full[id].release();
     }
-};
+}
 
 int main(){
-    std::cout << "\x1b[?25l"; //hide cursor
+    ios::sync_with_stdio(false);
+    cout << "\x1b[?25l"; //hide cursor
+
+    getTerminalSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     string output;
     int cur_fps=0;
     chrono::steady_clock::time_point fps_timer = std::chrono::steady_clock::now();
     start_time = std::chrono::steady_clock::now();
 
+    int renderheight = SCREEN_HEIGHT-1;
+    int block_size = renderheight / CORES;
+
+    vector<string> buffer(SCREEN_HEIGHT);
+    for(auto &line:buffer) line.reserve(SCREEN_WIDTH * 34 + 32);
+    array<thread,CORES> threads;
+
+    output.reserve(SCREEN_WIDTH * (SCREEN_HEIGHT - 1) * 34 + 196); // guess a safe size
+
+    for(int y = 0; y < CORES; y++){
+        threads[y] = thread(build_line, y*block_size, (y==CORES-1?renderheight:(y+1)*block_size), std::ref(buffer), y);
+    }
+
 
     for(int cur_frame = 0;;cur_frame++){
-        getTerminalSize(SCREEN_WIDTH, SCREEN_HEIGHT);
         delta_time_clock = std::chrono::steady_clock::now();
-        int delta_time = std::chrono::duration_cast<std::chrono::milliseconds>(delta_time_clock - fps_timer).count();
-        if(delta_time>=500){
+        if(cur_frame==1024){
+            int delta_time = std::chrono::duration_cast<std::chrono::milliseconds>(delta_time_clock - fps_timer).count();
             fps_timer = delta_time_clock;
-            cur_fps = cur_frame*2;
-            cur_frame = 0;
+            cur_fps = 1024/(float)delta_time*1000;
+            cur_frame=0;
         }
 
         ////////////////////////////////////////////
@@ -137,28 +177,18 @@ int main(){
         C.y = 25.0f+(sin(std::chrono::duration_cast<std::chrono::milliseconds>(delta_time_clock - start_time).count()/500.0f+1.0f))*12.0f;
 
 
-
         ////////////////////////////////////////////
 
-        vector<string> buffer(SCREEN_HEIGHT);
+        for(auto &s:semaphore_empty) s.release();
 
-        int CORES = 6;
-
-        int renderheight = SCREEN_HEIGHT-1;
-        int block_size = renderheight / CORES;
-
-        vector<thread> threads;
-
-        for(int y = 0; y < CORES; y++){
-            threads.emplace_back(build_line, y*block_size, (y==CORES-1?renderheight:(y+1)*block_size), std::ref(buffer));
-        }
-        for(auto &t : threads) t.join();
-
+        
         output.clear();
-        output.reserve(SCREEN_WIDTH * (SCREEN_HEIGHT - 1) * 23 + 196); // guess a safe size
 
         output += "\x1b[H\x1b[?25l";
         output += "\x1b[39;49m" + to_string(SCREEN_WIDTH) + "x" + to_string(SCREEN_HEIGHT*2) + " " + to_string(cur_fps) + "\x1b[K\n";
+
+        for(auto &s:semaphore_full) s.acquire();
+
         for(auto &i:buffer) output += i;
         cout << output;
         cout.flush();
